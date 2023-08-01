892c22d0fe49f6c0822567ca516e226c741f654d
Worlaz <wmalteskog@telia.com>
2023-08-01 11:57:58 +0200

    Added Platform Layer

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,8 @@
 
 int main(){
     
-    VkApplicationInfo appInfo;
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO; 
-    appInfo.pApplicationName = "Pong";
-    appInfo.pEngineName = "Pongine";
-
-
-    VkInstanceCreateInfo instanceInfo = {};
-    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    instanceInfo.pApplicationInfo = &appInfo;
     
-    VkInstance instance;
 
-    VkResult result = vkCreateInstance(&instanceInfo, 0, &instance);
-
-    if(result == VK_SUCCESS){
-        std::cout << "Successfully created vk instance" << std::endl;
-    }
-
+    
     return 0;
 }
